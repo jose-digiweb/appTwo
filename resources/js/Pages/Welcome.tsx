a3f47984c98a6a737602a72e85db59806af04dcf
jose-digiweb <josefurtado.digital@gmail.com>
2024-12-01 12:36:55 +0100

    firt

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,55 @@
+import { PageProps } from '@/types';
+import { Head, Link } from '@inertiajs/react';
+
+export default function Welcome({ auth }: PageProps) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <main className="flex h-full min-h-dvh flex-col bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+                <img
+                    id="background"
+                    className="absolute -left-20 top-0 max-w-[877px]"
+                    src="https://laravel.com/assets/img/welcome/background.svg"
+                />
+                <div className="flex min-h-dvh flex-col justify-between">
+                    <header className="flex w-full items-center justify-between p-8">
+                        <h1 className="text-4xl font-bold">JoseDigiweb</h1>
+                        <nav className="-mx-3 flex flex-1 justify-end">
+                            {auth.user ? (
+                                <Link
+                                    href={route('dashboard')}
+                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Dashboard
+                                </Link>
+                            ) : (
+                                <>
+                                    <Link
+                                        href={route('login')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log in
+                                    </Link>
+                                    <Link
+                                        href={route('register')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Register
+                                    </Link>
+                                </>
+                            )}
+                        </nav>
+                    </header>
+
+                    <main className="flex h-full flex-col items-center justify-center gap-8 p-8">
+                        <h1>Jose Furtado App</h1>
+                    </main>
+
+                    <footer className="flex items-center justify-center p-8">
+                        All rights reserved Jose Furtado
+                    </footer>
+                </div>
+            </main>
+        </>
+    );
+}
